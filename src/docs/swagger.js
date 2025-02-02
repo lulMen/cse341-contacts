3ba99992daf40f3748d104c71fe8a1b1f1cad9fc
ItsMen <mvdo1091@gmail.com>
2025-02-02 01:11:54 -0500

    updated swagger host

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -1,13 +1,16 @@
 const swaggerAutogen = require('swagger-autogen')();
 
 const PORT = process.env.PORT || 8080;
+const HOST = process.env.NODE_ENV === 'production'
+    ? 'https://cse341-contacts-rl68.onrender.com'
+    : `localhost:${PORT}`;
 
 const doc = {
     info: {
         title: 'My API',
         description: 'Description'
     },
-    host: `localhost:${PORT}`
+    host: HOST
 };
 
 const outputFile = './swagger-output.json';
